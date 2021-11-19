a3189409cb6e3b2407893a7a3c797dd13acdd513
Vladislav <vzv@siluetstudio.com>
2021-11-19 16:08:48 +0300

    files

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -7,9 +7,7 @@ $(document).ready(function() {
             let gridItemHeight = $(this).height();
 
             if (gridItemHeight > maxHeight) {
-                maxHeight = gridItemHeight;
-                console.log(maxHeight)
-               
+                maxHeight = gridItemHeight;               
             }
         })
 
@@ -31,4 +29,25 @@ $(document).ready(function() {
             $('body').removeClass('no-scroll');
         })
     }
+
+    //Маска телефона
+    if ($('.modal').length) {
+        $('input[name="phone"]').mask('+7(999)9999999')
+    }
+
+    //Успешная отправка
+    /*
+    Этот код скорее всего будет еще редактироваться, когда появится обработчик писем php
+    */
+    if ($('.modal').length) {
+        $('.form').on('submit', function(evt) {
+            evt.preventDefault();
+
+            $(this).find('.form__title').text('Спасибо');
+            $(this).find('.form__subtitle').text('Отдел поддержки клиентов свяжется с вами и уточнит информацию.')
+
+            $(this).find('.form__item, .form__submit, .form__agree').hide();
+        })
+    }
+    
 });
